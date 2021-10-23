06aaa7c8fc321294e853b117fcce14b998c47d7a
Calum Hay <calumhay@gmail.com>
2021-10-23 13:06:22 +1100

    connectModal contains test JSON request button

--- src/components/ConnectModal/index.js
+++ src/components/ConnectModal/index.js
@@ -2,6 +2,15 @@ import './ConnectModal.css';
 import { connectToSphero } from "../../utils/ble";
 
 const ConnectModal = (props) => {
+	var xmlhttp = new XMLHttpRequest();
+	var url = "http://raspberrypi:8080/";
+
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var myArr = JSON.parse(this.responseText);
+			console.log(myArr);
+		}
+	};
 
 	return (
 		<div className="modal">
@@ -9,6 +18,10 @@ const ConnectModal = (props) => {
 				<div>
 					<h2>Sphero not connected.</h2>
 					<input className="button" type="button" value="Connect" onClick={() => connectToSphero(props.onConnected, props.onDisconnected)} />
+					<input className="button" type="button" value="Get JSON" onClick={() => {
+						xmlhttp.open("GET", url, true);
+						xmlhttp.send();
+					}} />
 				</div>
 			</div>
 		</div>
